cd4acea7d481332a5da646bb09853316be985a98
Nikhil-Suryawanshi07 <suryawanshinikhil73@gmail.com>
2026-06-01 18:04:17 +0530

    update

--- node.groovy
+++ node.groovy
@@ -1,86 +1,77 @@
 pipeline {
     agent any
-    
+
     tools {
         nodejs 'NodeJS-20'
     }
-
     environment {
-        IMAGE_NAME = "node-demo-app"
-        DOCKER_REPO = "mayurmwagh/node-demo-sample"
-        CONTAINER_NAME = "node-demo-container"
+        IMAGE_NAME = 'my-node-app'
+        DOCKER_REPO = 'my-docker-repo'
+        CONTAINER_NAME = 'my-node-container'
     }
     stages {
-        stage('checkout') {
+        stage('Checkout') {
             steps {
                 git branch: 'main',
-                    url: 'https://github.com/mayurmwagh/node-app.git'
+                 url: 'https://github.com/Nikhil-Suryawanshi07/node-app.git'
+                 
             }
-        }
-    
-        stage('Verify Environment') {
+        }           
+        stage('verify Environment') {
             steps {
                 sh '''
                 echo "Node Version:"
                 node -v
 
                 echo "NPM Version:"
-                npm -v
+                node -v
 
                 echo "Docker Version:"
                 docker --version
+
                 '''
             }
         }
-        stage('Install Dependencies') {
+        stage('install dependencies'){
             steps {
-                sh 'npm install' 
+                sh 'npm install'
             }
         }
-        stage('Run Tests') {
+        stage('run tests') {
             steps {
                 sh 'npm test'
             }
         }
-        stage('Build Docker Image') {
+        stage('build docker image') {
             steps {
-                sh '''
-                docker build -t ${DOCKER_REPO}:${BUILD_NUMBER} .
-                ''' 
+               sh '''docker build -t { DOCKER_REPO }:${BUILD_NUMBER} .'''
             }
         }
-        stage('Docker Login') {
-            steps {
+        stage('docker Login') {
+            steps{
                 withCredentials([
                     usernamePassword(
                         credentialsId: 'dockerhub-creds',
                         usernameVariable: 'DOCKER_USERNAME',
-                        passwordVariable: 'DOCKER_PASSORD'
+                        passwordVariable: 'DOCKER_PASSWORD'
                     )
-                ]) {
-                    sh 'docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}'
+                ]){
+                    sh 'docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD'
+                }
                 }
             }
-        }
-        stage('Push Docker Image') {
+        stage('push docker image') {
             steps {
-                sh '''
-                docker push ${DOCKER_REPO}:${BUILD_NUMBER}
-                '''
+                sh ''' docker push ${DOCKER_REPO}:${BUILD_NUMBER} '''
             }
         }
-        stage('Deploy Container') {
+        stage('deploy to docker container') {
             steps {
                 sh '''
                 docker rm -f ${CONTAINER_NAME} || true
-
-                docker run -d \
-                --name ${CONTAINER_NAME} \
-                -p 3000:3000 \
-                ${DOCKER_REPO}:${BUILD_NUMBER}
+                docker run -d -p 3000:3000 --name ${CONTAINER_NAME} ${DOCKER_REPO}:${BUILD_NUMBER}  
                 '''
             }
         }
     }
-}       
-
+}
