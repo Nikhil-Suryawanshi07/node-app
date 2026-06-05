2a4e017fe12e52953bbe46cd066900ad1adc7b8a
Nikhil-Suryawanshi07 <suryawanshinikhil73@gmail.com>
2026-06-05 15:42:48 +0530

    do

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Welcome to NodeJS CI/CD Pipeline Demo and Dockerization to Wardha");
+    res.send("Welcome to NodeJS CI/CD Pipeline Demo and Dockerization to Wardha..");
 });
 
 app.get("/health", (req, res) => {
